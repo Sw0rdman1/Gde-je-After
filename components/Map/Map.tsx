41cbde36377738c9e01362ae5f58da19ed0d4d12
OfficeB <office@ih-club.com>
2025-02-20 14:31:36 +0100

    feat: add dynamic map padding based on selected party in Map component

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -4,6 +4,7 @@ import { StyleSheet } from 'react-native'
 import MapView, { CameraZoomRange } from 'react-native-maps'
 import PartyMarker from './PartyMarker'
 import { useApp } from '@/context/AppProvider'
+import { useMemo } from 'react'
 
 const CAMERA_ZOOM_RANGE: CameraZoomRange = {
     minCenterCoordinateDistance: 100,
@@ -13,15 +14,19 @@ const CAMERA_ZOOM_RANGE: CameraZoomRange = {
 
 const Map = () => {
     const { currentLocation } = useLocation()
-    const { parties } = useParty()
+    const { parties, selectedParty } = useParty()
     const { mapRef } = useApp()
+
+    const mapPadding = useMemo(() => {
+        const bottom = selectedParty ? 300 : 0
+        return { bottom, right: 0, top: 0, left: 0 }
+    }, [selectedParty])
 
     const renderMarkers = () => {
         return parties.map((party, index) => (
             <PartyMarker key={party.id} party={party} index={index} />
         ))
     }
-
 
     return (
         <MapView
@@ -31,6 +36,7 @@ const Map = () => {
             initialRegion={currentLocation}
             showsUserLocation={true}
             loadingEnabled={true}
+            mapPadding={mapPadding}
             cameraZoomRange={CAMERA_ZOOM_RANGE}
 
         >
